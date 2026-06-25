f583ef967400320721dc84f998d75619213bc579
Ben-hur Veber <benhurmattosv@gmail.com>
2026-06-25 16:30:46 -0300

    Add files via upload

---
+++ src/GerenciamentoVendas/VisualizacaoPedidos.jsx
@@ -0,0 +1,39 @@
+import { useState } from "react"
+
+export default function VisualizacaoPedidos(props) {
+    const [exibirModal, setExibirModal] = useState(false)
+    const [pedidoSelecionado, setPedidoSelecionado] = useState('')
+    const [exibirInfo, setExibirInfo] = useState(false)
+
+    return (
+        <div>
+            <button onClick={() => setExibirModal(true)}>Visualizar pedidos</button>
+            {exibirModal === false ? null:
+            <dialog open>
+
+                <label>Selecione um pedido: </label>
+                <select value={pedidoSelecionado} onChange={(valorId) => {
+                    let pedidoRecuperado = props.pedidos.find(pedido => pedido.id === valorId.target.value)
+                    setPedidoSelecionado(pedidoRecuperado)
+                }}>
+
+                    <option value="" disabled>Escolha um pedido</option>
+                    {props.pedidos.map(pedido => <option key={pedido.id} value={pedido.id} >ID: {pedido.id}, Data: {pedido.data_pedido}</option>)}
+                    
+                </select>
+
+                <br /><br />
+
+                {exibirInfo ? 
+                <div>
+                    <h1>Dados do Cliente</h1><br />
+                    <label>Nome:</label> {}
+
+                </div>
+                : null}
+                <button onClick={() => setExibirModal(false)}>Fechar</button>
+            </dialog>
+            }
+        </div>
+    )
+}
